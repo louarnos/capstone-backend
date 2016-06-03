ef429ac060bb6104c29a981e05a0a3ba6800dbc3
louarnos <louarnos@gmail.com>
2016-06-03 15:54:40 -0400

    Edited user schema
    To include username
    
     - required true
     - unique true

--- app/models/user.js
+++ app/models/user.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
     unique: true,
     required: true,
   },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   token: {
     type: String,
     require: true,
